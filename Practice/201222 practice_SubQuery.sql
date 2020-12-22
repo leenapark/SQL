2d7497393286ae89cbeed738663bce3ce50ac959
leenapark <hrn424@gmail.com>
2020-12-23 00:37:50 +0900

    20201222 실습문제_subQuery

--- Practice/201222 practice_SubQuery.sql
+++ Practice/201222 practice_SubQuery.sql
@@ -128,5 +128,124 @@ from employees
 GROUP BY department_id;
 
 --조건절비교
-select  *
-from employees;
+select  em.employee_id,
+        em.first_name,
+        em.salary,
+        em.department_id
+from employees em
+where (em.department_id, em.salary) in (select  department_id,
+                                                max(salary)
+                                        from employees
+                                        GROUP BY department_id)
+order by salary asc;
+
+--테이블조인
+select  em.employee_id,
+        em.first_name,
+        em.salary,
+        em.department_id
+from employees em, (select  department_id,  
+                            max(salary) salary
+                    from employees
+                    GROUP BY department_id) se
+where em.salary = se.salary
+and em.department_id = se.department_id;
+
+
+/*
+문제6.
+각 업무(job) 별로 연봉(salary)의 총합을 구하고자 합니다. 
+연봉 총합이 가장 높은 업무부터 업무명(job_title)과 연봉 총합을 조회하시오 
+(19건)
+*/
+--업무별 연봉 총합
+select  em.job_id,
+        sum(em.salary)
+from employees em
+group by em.job_id;
+
+select  jo.job_title,
+        em.salary
+from employees em, jobs jo
+where (jo.job_id, em.salary) in (select  emp.job_id,
+                                         sum(emp.salary)
+                                from employees emp
+                                group by emp.job_id);
+                            
+--table join
+select  j.job_title,
+        e.salary
+from employees e, jobs j, (select  emp.job_id,
+                                   sum(emp.salary) salary
+                                from employees emp
+                                group by emp.job_id) s
+where e.job_id = j.job_id
+and e.salary = s.salary;
+                                
+
+/*
+문제7.
+자신의 부서 평균 급여보다 연봉(salary)이
+많은 직원의 직원번호(employee_id),
+이름(first_name)과
+급여(salary)을 조회하세요 
+(38건)
+*/
+--부서 평균 급여
+select  department_id,
+        avg(salary) avg
+from employees
+group by department_id;
+
+/*실패 재도전 해보기
+select  em.employee_id,
+        em.first_name "이름",
+        em.salary "급여"
+from employees em
+where em.salary >any (select avg(mpl.salary)
+                      from employees mpl
+                      )
+                      and em.department_id =any (select  e.department_id de
+                      from employees e
+                      group by e.department_id);
+*/                   
+
+select  em.employee_id,
+        em.first_name "이름",
+        em.salary "급여"
+from employees em, (select  e.department_id,
+                            avg(salary) avg
+                            from employees e
+                            group by department_id) s
+where em.department_id = s.department_id
+and em.salary > s.avg;
+
+
+/*
+문제8.
+직원 입사일이 11번째에서 15번째의
+직원의 사번,
+이름,
+급여,
+입사일을
+입사일 순서로 출력하세요
+*/
+select  rn,
+        employee_id,
+        first_name,
+        salary,
+        hire_date
+        from(select rownum rn,  --조건문
+                    employee_id,
+                    first_name,
+                    salary,
+                    hire_date
+                    from(select employee_id,
+                                first_name,
+                                salary,
+                                hire_date
+                         from employees
+                         order by hire_date asc)    --가장 처음 조건을 생성 = 정렬 & 실행 내용
+                         )
+where rn >=11
+and rn <=15;        --rownum 조건은 where절로 생성
